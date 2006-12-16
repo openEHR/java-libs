7967034ba891cdaf715aecb0f77fe088e8cbd90a
rong.chen <rong.chen@cambio.se>
2006-12-16 21:14:04 +0000

    updated CDvCodedText support regarding query

--- adl-parser/src/test/java/se/acode/openehr/parser/CDvCodedTextTest.java
+++ adl-parser/src/test/java/se/acode/openehr/parser/CDvCodedTextTest.java
@@ -34,42 +34,46 @@ import java.util.*;
 
 public class CDvCodedTextTest extends ParserTestBase {
 
-    public CDvCodedTextTest(String test) throws Exception {
-        super(test);
-    }
+	public CDvCodedTextTest(String test) throws Exception {
+		super(test);
+	}
 
-    /**
-     * The fixture set up called before every test method.
-     */
-    protected void setUp() throws Exception {
-        ADLParser parser = new ADLParser(loadFromClasspath(
-                "adl-test-entry.c_dv_coded_text.test.adl"));
-        archetype = parser.parse();
-    }
+	/**
+	 * The fixture set up called before every test method.
+	 */
+	protected void setUp() throws Exception {
+		ADLParser parser = new ADLParser(
+				loadFromClasspath("adl-test-entry.c_dv_coded_text.test.adl"));
+		archetype = parser.parse();
+	}
 
-    /**
-     * The fixture clean up called after every test method.
-     */
-    protected void tearDown() throws Exception {
-        archetype = null;
-    }
+	/**
+	 * The fixture clean up called after every test method.
+	 */
+	protected void tearDown() throws Exception {
+		archetype = null;
+	}
 
-    public void testCCodedText() throws Exception {
-        CObject node = archetype.node(
-                "/types[at0001]/items[at10002]/value");
-        assertTrue("CCodedText expected", node instanceof CDvCodedText);
-        CDvCodedText CDvCodedText = (CDvCodedText) node;
-        assertEquals("terminology", "local", 
-        		CDvCodedText.getTerminologyId().getValue());
-        String[] codes = {
-            "at2001.0", "at2001.1", "at2001.2"
-        };
-        List<String> codeList = CDvCodedText.getCodeList();
-        assertEquals("codes.size", codes.length, codeList.size());
-        for(int i = 0; i < codes.length; i++) {
-            assertEquals("code", codes[i], codeList.get(i));
-        }
-    } 
+	public void testParseCDvCodedText() throws Exception {
+		CObject node = archetype.node("/types[at0001]/items[at10002]/value");
+		assertTrue("CDvCodedText expected", node instanceof CDvCodedText);
+		CDvCodedText CDvCodedText = (CDvCodedText) node;
+		assertEquals("terminology", "local", CDvCodedText.getTerminologyId()
+				.getValue());
+		String[] codes = { "at2001.0", "at2001.1", "at2001.2" };
+		List<String> codeList = CDvCodedText.getCodeList();
+		assertEquals("codes.size", codes.length, codeList.size());
+		for (int i = 0; i < codes.length; i++) {
+			assertEquals("code", codes[i], codeList.get(i));
+		}
 
-    private Archetype archetype;
+		node = archetype.node("/types[at0001]/items[at10003]/value");
+		assertTrue("CDvCodedText expected", node instanceof CDvCodedText);
+		CDvCodedText = (CDvCodedText) node;
+		assertEquals("terminology", null, CDvCodedText.getTerminologyId());
+		assertEquals("codeList", null, CDvCodedText.getCodeList());
+		assertEquals("query", "ac2001", CDvCodedText.getQuery());
+	}
+
+	private Archetype archetype;
 }
