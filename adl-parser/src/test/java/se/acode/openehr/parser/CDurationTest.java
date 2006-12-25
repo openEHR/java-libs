057effd397ed3a386aeec50ffb0ba4c6e705d5ab
rong.chen <rong.chen@cambio.se>
2006-12-25 03:19:16 +0000

    added support for duration interval in CDuration parsing

--- adl-parser/src/test/java/se/acode/openehr/parser/CDurationTest.java
+++ adl-parser/src/test/java/se/acode/openehr/parser/CDurationTest.java
@@ -1,6 +1,8 @@
 package se.acode.openehr.parser;
 
 import org.openehr.am.archetype.Archetype;
+import org.openehr.am.archetype.constraintmodel.CPrimitiveObject;
+import org.openehr.am.archetype.constraintmodel.primitive.CDuration;
 import org.openehr.rm.datatypes.quantity.datetime.DvDuration;
 import org.openehr.rm.support.basic.Interval;
 
@@ -23,7 +25,7 @@ public class CDurationTest extends ParserTestBase {
 	 * 
 	 * @throws Exception
 	 */
-	public void testDurationConstraint() throws Exception {
+	public void testParseCDuration() throws Exception {
 
 		assertCDuration(archetype.node("/types[at0001]/items[at1001]/value"), 
 				"PT0s", null);
@@ -57,29 +59,49 @@ public class CDurationTest extends ParserTestBase {
 
 		// to supported newly added duration pattern
 		assertCDuration(archetype.node("/types[at0001]/items[at1009]/value"), 
-				null, null, null, "PDTH");
-		
-		// assumed values
+				null, null, null, "PDTH");		
+	}
+	
+	/**
+	 * Verifies the support for "|PT10M|", single duration interval
+	 */
+	public void testParseSingleDurationInverval() throws Exception {
+		Interval<DvDuration> interval = new Interval<DvDuration> (
+				DvDuration.getInstance("PT10M"), null);
 		assertCDuration(archetype.node("/types[at0001]/items[at1010]/value"), 
+				null, interval, null, null);
+		
+		// test with assumed value
+		assertCDuration(archetype.node("/types[at0002]/items[at1010]/value"), 
+				null, interval, "PT12M", null);
+	}
+	
+	/**
+	 * Tests parsing CDurations with assumed values
+	 * 
+	 * @throws Exception
+	 */
+	public void testParseCDurationWithAssumedValue() throws Exception {
+		assertCDuration(archetype.node("/types[at0002]/items[at1001]/value"), 
 				"PT0s", null, "P1d");
 
-		assertCDuration(archetype.node("/types[at0001]/items[at1011]/value"), 
+		assertCDuration(archetype.node("/types[at0002]/items[at1002]/value"), 
 				"P1d", null, "P1d");
 
-		assertCDuration(archetype.node("/types[at0001]/items[at1012]/value"), 
+		assertCDuration(archetype.node("/types[at0002]/items[at1003]/value"), 
 				"PT2h5m0s",	null, "P1d");
 
-		assertCDuration(archetype.node("/types[at0001]/items[at1013]/value"), 
+		assertCDuration(archetype.node("/types[at0002]/items[at1004]/value"), 
 				null,
 				new Interval<DvDuration>(DvDuration.getInstance("PT1h55m0s"), 
 						DvDuration.getInstance("PT2h5m0s")), "P1d");
 
-		assertCDuration(archetype.node("/types[at0001]/items[at1014]/value"), 
+		assertCDuration(archetype.node("/types[at0002]/items[at1005]/value"), 
 				null,
 				new Interval<DvDuration>(null, DvDuration.getInstance("PT1h"), 
 						false, true), "P1d");
 		// to supported newly added duration pattern
-		assertCDuration(archetype.node("/types[at0001]/items[at1015]/value"), 
+		assertCDuration(archetype.node("/types[at0002]/items[at1006]/value"), 
 				null, null, "P1d", "PDTH");
 	}
 
