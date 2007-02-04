7267579df2b997f7c2ffb57d1f4e17e63e61568c
rong.chen <rong.chen@cambio.se>
2007-02-04 23:23:48 +0000

    testcase verifies node paths after parsing

--- adl-parser/src/test/java/se/acode/openehr/parser/PathTest.java
+++ adl-parser/src/test/java/se/acode/openehr/parser/PathTest.java
@@ -19,6 +19,7 @@
 
 package se.acode.openehr.parser;
 
+import org.openehr.am.archetype.Archetype;
 import org.openehr.am.archetype.constraintmodel.CComplexObject;
 import org.openehr.am.archetype.constraintmodel.CObject;
 import org.openehr.am.archetype.constraintmodel.CAttribute;
@@ -38,7 +39,8 @@ public class PathTest extends ParserTestBase {
     public void setUp() throws Exception {
         ADLParser parser = new ADLParser(loadFromClasspath(
                 "adl-test-car.paths.test.adl"));
-        definition = parser.parse().getDefinition();
+        archetype = parser.parse();
+        definition = archetype.getDefinition();
     }
 
     public void testPath() throws Exception {
@@ -49,7 +51,7 @@ public class PathTest extends ParserTestBase {
         // wheels attribute
         CAttribute wheels = definition.getAttributes().get(0);
         assertEquals("wheels", "/wheels", wheels.path());
-
+        
         // first WHEEL node
         CObject firstWheel = wheels.getChildren().get(0);
         assertEquals("first wheel", "/wheels[at0001]",
@@ -79,6 +81,53 @@ public class PathTest extends ParserTestBase {
                 "/wheels[at0001]/parts[at0002]/something_else",
                 wheelPartsObj.getAttributes().get(1).path());
     }
+    
+    public void testNodeAtPath() throws Exception {
+    	String[] paths = {
+    			"/", 
+    			"/wheels[at0001]", 
+    			"/wheels[at0001]/description",
+    			"/wheels[at0001]/parts[at0002]",
+    			"/wheels[at0001]/parts[at0002]/something",
+    			"/wheels[at0001]/parts[at0002]/something_else",
+    			"/wheels[at0003]", 
+    			"/wheels[at0003]/description",
+    			"/wheels[at0004]",
+    			"/wheels[at0004]/description",
+    			"/wheels[at0005]", 
+    			"/wheels[at0005]/description"
+    	};
+    	
+    	CAttribute wheels = definition.getAttributes().get(0);
+    	CComplexObject wheel1 = ((CComplexObject) wheels.getChildren().get(0));
+    	CComplexObject wheel2 = ((CComplexObject) wheels.getChildren().get(1));
+    	CComplexObject wheel3 = ((CComplexObject) wheels.getChildren().get(2));
+    	CComplexObject wheel4 = ((CComplexObject) wheels.getChildren().get(3));
+    	CComplexObject parts = ((CComplexObject) wheel1.getAttributes().get(1)
+    								.getChildren().get(0));   			
+    	
+    	
+    	CObject[] nodes = {
+    			definition, 
+    			wheel1,
+    			wheel1.getAttributes().get(0).getChildren().get(0),
+    			parts,
+    			parts.getAttributes().get(0).getChildren().get(0),
+    			parts.getAttributes().get(1).getChildren().get(0),
+    			wheel2,
+    			wheel2.getAttributes().get(0).getChildren().get(0),
+    			wheel3,
+    			wheel3.getAttributes().get(0).getChildren().get(0),
+    			wheel4,
+    			wheel4.getAttributes().get(0).getChildren().get(0),    			
+    	};
+    			
+    	for(int i = 0; i < paths.length; i++) {
+    		assertEquals("wrong at path: " + paths[i], nodes[i], 
+    				archetype.node(paths[i]));
+    	}
+    }
 
+    private Archetype archetype;
     private CComplexObject definition;
 }
