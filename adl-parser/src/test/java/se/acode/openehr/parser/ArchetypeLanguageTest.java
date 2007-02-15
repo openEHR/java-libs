c2690155de22f46ecf369dc2471f61327978a8a9
rong.chen <rong.chen@cambio.se>
2007-02-15 15:06:17 +0000

    fixed parsing translations without accreditation

--- adl-parser/src/test/java/se/acode/openehr/parser/ArchetypeLanguageTest.java
+++ adl-parser/src/test/java/se/acode/openehr/parser/ArchetypeLanguageTest.java
@@ -20,46 +20,70 @@ import org.openehr.rm.common.resource.TranslationDetails;
 
 /**
  * Testcase for Archetype language section parsing
- *
+ * 
  * @author Rong Chen
  * @version 1.0
  */
 public class ArchetypeLanguageTest extends ParserTestBase {
 
-    /**
-     * Create new test case
-     *
-     * @param test
-     * @throws Exception
-     */
-    public ArchetypeLanguageTest(String test) throws Exception {
-        super(test);
-    }
+	/**
+	 * Create new test case
+	 * 
+	 * @param test
+	 * @throws Exception
+	 */
+	public ArchetypeLanguageTest(String test) throws Exception {
+		super(test);
+	}
 
-    public void testParse() throws Exception {
-        ADLParser parser = new ADLParser(loadFromClasspath(
-                "adl-test-entry.archetype_language.test.adl"));
-        Archetype archetype = parser.parse();
-        assertNotNull(archetype);
-        
-        Map<String, TranslationDetails> translations = 
-        	archetype.getTranslations();
-        assertNotNull("translations null");
-        
-        TranslationDetails td = translations.get("de");
-        assertNotNull("translation de missing", td);
-        Map<String, String> map = td.getAuthor();
-        assertNotNull("author null", map);
-        assertEquals("author.name wrong", "Harry Potter", map.get("name"));
-        assertEquals("author.email wrong", "harry@something.somewhere.co.uk", 
-        		map.get("email"));
-        
-        assertEquals("acrreditation wrong", 
-        		"British Medical Translator id 00400595", td.getAccreditation());
-        
-        map = td.getOtherDetails();
-        assertEquals("review 1 wrong", "Ron Weasley", map.get("review 1"));
-        assertEquals("review 2 wrong", "Rubeus Hagrid", map.get("review 2"));        
-    }
+	public void testParseLanguageSection() throws Exception {
+		ADLParser parser = new ADLParser(
+				loadFromClasspath("adl-test-entry.archetype_language.test.adl"));
+		Archetype archetype = parser.parse();
+		assertNotNull(archetype);
+
+		Map<String, TranslationDetails> translations = archetype
+				.getTranslations();
+		assertNotNull("translations null");
+
+		TranslationDetails td = translations.get("de");
+		assertNotNull("translation de missing", td);
+		Map<String, String> map = td.getAuthor();
+		assertNotNull("author null", map);
+		assertEquals("author.name wrong", "Harry Potter", map.get("name"));
+		assertEquals("author.email wrong", "harry@something.somewhere.co.uk",
+				map.get("email"));
+
+		assertEquals("acrreditation wrong",
+				"British Medical Translator id 00400595", td.getAccreditation());
+
+		map = td.getOtherDetails();
+		assertEquals("review 1 wrong", "Ron Weasley", map.get("review 1"));
+		assertEquals("review 2 wrong", "Rubeus Hagrid", map.get("review 2"));
+	}
+
+	public void testParseLanguageWithoutAccreditation() throws Exception {
+		ADLParser parser = new ADLParser(loadFromClasspath(
+				"adl-test-entry.archetype_language_no_accreditation.test.adl"));
+		Archetype archetype = parser.parse();
+		assertNotNull(archetype);
+
+		Map<String, TranslationDetails> translations = archetype
+				.getTranslations();
+		assertNotNull("translations null");
+
+		TranslationDetails td = translations.get("de");
+		assertNotNull("translation de missing", td);
+		Map<String, String> map = td.getAuthor();
+		assertNotNull("author null", map);
+		assertEquals("author.name wrong", "Harry Potter", map.get("name"));
+		assertEquals("author.email wrong", "harry@something.somewhere.co.uk",
+				map.get("email"));
+
+		assertEquals("acrreditation wrong", null, td.getAccreditation());
+
+		map = td.getOtherDetails();
+		assertEquals("review 1 wrong", "Ron Weasley", map.get("review 1"));
+		assertEquals("review 2 wrong", "Rubeus Hagrid", map.get("review 2"));
+	}
 }
-
