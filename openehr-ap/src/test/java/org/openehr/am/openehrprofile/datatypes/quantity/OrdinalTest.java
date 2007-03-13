23635490e8842c6426c356777c198b944555835a
rong.chen <rong.chen@cambio.se>
2007-03-13 23:22:24 +0000

    allow ordinal with negative value

--- openehr-ap/src/test/java/org/openehr/am/openehrprofile/datatypes/quantity/OrdinalTest.java
+++ openehr-ap/src/test/java/org/openehr/am/openehrprofile/datatypes/quantity/OrdinalTest.java
@@ -11,4 +11,12 @@ public class OrdinalTest extends TestCase {
 		assertTrue("equals expected", o1.equals(o2));
 		assertTrue("equals expected", o2.equals(o1));
 	}
+	
+	public void testCreateOrdinalWithNegativeValue() {
+		try {
+			new Ordinal(-1, new CodePhrase("local", "at0001"));
+		} catch(Exception e) {
+			fail("failed to create ordinal with negative value");
+		}
+	}
 }
