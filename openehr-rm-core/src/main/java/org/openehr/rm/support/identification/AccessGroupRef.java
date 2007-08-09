1c49c676392fada3559b28c6e3ed37f5639554e2
rong.chen <rong.chen@cambio.se>
2007-08-09 23:31:25 +0000

    added class AccessGroupRef

---
+++ openehr-rm-core/src/main/java/org/openehr/rm/support/identification/AccessGroupRef.java
@@ -0,0 +1,61 @@
+/*
+ * component:   "openEHR Reference Implementation"
+ * description: "Class AccessGroupRef"
+ * keywords:    "datatypes"
+ *
+ * author:      "Rong Chen <rong@acode.se>"
+ * copyright:   "Copyright (c) 2007 Cambio Healthcare Systems, Sweden"
+ * license:     "See notice at bottom of class"
+ *
+ * file:        "$URL$"
+ * revision:    "$LastChangedRevision$"
+ * last_change: "$LastChangedDate$"
+ */
+package org.openehr.rm.support.identification;
+
+/**
+ * Reference to access group in an access control service
+ * 
+ * @author Rong Chen
+ */
+public class AccessGroupRef extends ObjectRef {
+	/**
+     * Construt an AccessGroupRef
+     *
+     * @param id
+     * @throws IllegalArgumentException if id or type null
+     */
+    public AccessGroupRef(ObjectID id) {
+        super(id, ObjectRef.Namespace.ACCESS_CONTROL, 
+        		ObjectRef.Type.ACCESS_GROUP);
+    }    
+}
+/*
+ *  ***** BEGIN LICENSE BLOCK *****
+ *  Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ *  The contents of this file are subject to the Mozilla Public License Version
+ *  1.1 (the 'License'); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.mozilla.org/MPL/
+ *
+ *  Software distributed under the License is distributed on an 'AS IS' basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  The Original Code is AccessGroupRef.java
+ *
+ *  The Initial Developer of the Original Code is Rong Chen.
+ *  Portions created by the Initial Developer are Copyright (C) 2003-2007
+ *  the Initial Developer. All Rights Reserved.
+ *
+ *  Contributor(s):
+ *
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ *  ***** END LICENSE BLOCK *****
+ */
