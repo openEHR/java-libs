6ca63e436562e77116552d26477e0395ac1e3bdc
rong.chen <rong.chen@cambio.se>
2008-09-18 12:01:22 +0000

    fixed unnecessary depenedency on the order of language and author elements in the archetype language translations parsing

--- adl-parser/src/test/java/se/acode/openehr/parser/ArchetypeLanguageTest.java
+++ adl-parser/src/test/java/se/acode/openehr/parser/ArchetypeLanguageTest.java
@@ -86,4 +86,37 @@ public class ArchetypeLanguageTest extends ParserTestBase {
 		assertEquals("review 1 wrong", "Ron Weasley", map.get("review 1"));
 		assertEquals("review 2 wrong", "Rubeus Hagrid", map.get("review 2"));
 	}
+	
+	public void testParseTranslationsLanguageAuthor() throws Exception {
+		ADLParser parser = new ADLParser(loadFromClasspath(
+				"adl-test-entry.translations_language_author.test.adl"));
+		Archetype archetype = parser.parse();
+		assertNotNull(archetype);
+
+		Map<String, TranslationDetails> translations = archetype
+				.getTranslations();
+		assertNotNull("translations null");
+
+		TranslationDetails td = translations.get("de");
+		assertNotNull("translation de missing", td);
+		Map<String, String> map = td.getAuthor();
+		assertNotNull("author null", map);	
+	}
+	
+	// test the reverse order of language, author in translations
+	public void testParseTranslationsAuthorLanguage() throws Exception {
+		ADLParser parser = new ADLParser(loadFromClasspath(
+				"adl-test-entry.translations_author_language.test.adl"));
+		Archetype archetype = parser.parse();
+		assertNotNull(archetype);
+
+		Map<String, TranslationDetails> translations = archetype
+				.getTranslations();
+		assertNotNull("translations null");
+
+		TranslationDetails td = translations.get("de");
+		assertNotNull("translation de missing", td);
+		Map<String, String> map = td.getAuthor();
+		assertNotNull("author null", map);	
+	}
 }
