8e6ad5d3db23ebfc009e58e26ffff19d0ba546a4
sebastian.garde <sebastian.garde@oceaninformatics.com>
2010-05-07 15:11:27 +0000

    Add support for uncommon keys in Archetype Term (Thanks to Saso Rutar)

---
+++ adl-parser/src/test/java/se/acode/openehr/parser/ArchetypeUncommonTermKeysTest.java
@@ -0,0 +1,36 @@
+package se.acode.openehr.parser;
+
+import java.util.*;
+
+import org.openehr.am.archetype.Archetype;
+import org.openehr.rm.common.resource.ResourceDescription;
+import org.openehr.rm.common.resource.ResourceDescriptionItem;
+import org.openehr.rm.datatypes.text.CodePhrase;
+import org.openehr.am.archetype.ontology.ArchetypeTerm;
+
+
+/**
+ * Testcase for uncommon term keys (other than text, description and comment)
+ *
+ * @author Sebastian Garde
+ * @version 1.0
+ */
+public class ArchetypeUncommonTermKeysTest extends ParserTestBase {
+
+	/**
+	 * Verifies the content of description instance after parsing
+	 * 
+	 * @throws Exception
+	 */
+	public void testArchetypeUncommonTerm() throws Exception {
+		ADLParser parser = new ADLParser(
+				loadFromClasspath("adl-test-entry.archetype_uncommonkeys.test.adl"));
+		Archetype archetype = parser.parse();
+		ArchetypeTerm aterm = archetype.getOntology().termDefinition("at0000");
+	
+		assertEquals("key value wrong", "another key value", aterm.getItem("anotherkey"));
+	}
+
+	
+}
+
