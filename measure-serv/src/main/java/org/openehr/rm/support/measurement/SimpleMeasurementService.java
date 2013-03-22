ba1e848da239881cf4129443bfc61b53dea3b6f5
U-CAMBIO\iago.corbal <iago.corbal@CWS-IAGO.cambio.se>
2013-03-22 17:12:43 +0100

    Measurement service. Equal units fix.

--- measure-serv/src/main/java/org/openehr/rm/support/measurement/SimpleMeasurementService.java
+++ measure-serv/src/main/java/org/openehr/rm/support/measurement/SimpleMeasurementService.java
@@ -123,6 +123,7 @@ public class  SimpleMeasurementService implements MeasurementService {
 	    return measure1.compareTo(measure2);
 	}
 }
+}
 /*
  *  ***** BEGIN LICENSE BLOCK *****
  *  Version: MPL 1.1/GPL 2.0/LGPL 2.1
