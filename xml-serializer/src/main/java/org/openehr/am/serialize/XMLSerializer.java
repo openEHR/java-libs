openEHR/java-libs

commit 88a92007eab87aa05ec322935943f0ab9733d660

Merging with pubic repository

xml-serializer/src/main/java/org/openehr/am/serialize/XMLSerializer.java
@@ -263,12 +263,12 @@ public class XMLSerializer {
     private void printStringMap(String label, Map<String, String> map, Element out) {
 
         if(map != null && !map.isEmpty()) {
-            for(String key : map.keySet()) {
+            for(Map.Entry <String, String> entry : map.entrySet()) {
                 Element elm = new Element(label, defaultNamespace);
                 out.getChildren().add(elm);
-                elm.setAttribute("id", key);
-                if (map.get(key) != null) {
-                    elm.setText(map.get(key));
+                elm.setAttribute("id", entry.getKey());
+                if (entry.getValue() != null) {
+                    elm.setText(map.get(entry.getValue()));
                 }
             }
         }

adl-serializer/src/main/java/org/openehr/am/serialize/ADLSerializer.java
@@ -45,11 +45,11 @@ import java.util.Set;
 
 /**
  * ADL serializer for the openEHR Java kernel
- * 
+ *
  * @author Rong Chen
  * @author Mattias Forss, Johan Hjalmarsson
  * @author Sebastian Garde
- * 
+ *
  * @version 1.0
  */
 public class ADLSerializer {
@@ -65,7 +65,7 @@ public class ADLSerializer {
 
 	/**
 	 * Output given archetype as string in ADL format
-	 * 
+	 *
 	 * @param archetype
 	 * @return a string in ADL format
 	 * @throws IOException
@@ -76,9 +76,9 @@ public class ADLSerializer {
 		return writer.toString();
 	}
 
-		/**
+	/**
 	 * Output  archetype DEFINITION as string in ADL format
-	 * 
+	 *
 	 * @param archetype
 	 * @return a string in ADL format
 	 * @throws IOException
@@ -87,13 +87,13 @@ public class ADLSerializer {
 		StringWriter writer = new StringWriter();
 
 		printDefinition(archetype.getDefinition(), writer);
-				
+
 		return writer.toString();
 	}
-	
+
 	/**
 	 * Output given archetype to outputStream
-	 * 
+	 *
 	 * @param archetype
 	 * @param out
 	 * @throws IOException
@@ -107,40 +107,40 @@ public class ADLSerializer {
 
 	/**
 	 * Output given archetype to writer
-	 * 
+	 *
 	 * @param archetype
 	 * @param out
 	 * @throws IOException
 	 */
 	public void output(Archetype archetype, Writer out) throws IOException {
-		printHeader(archetype.getAdlVersion(), archetype.getArchetypeId(), 
+		printHeader(archetype.getAdlVersion(), archetype.getArchetypeId(),
 				archetype.getParentArchetypeId(), archetype.getUid(), archetype.getConcept(), out);
 		newline(out);
-		
+
 		printLanguage(archetype, out);
 		newline(out);
-		
+
 		if(archetype.getDescription() != null) {
 			printDescription(archetype.getDescription(), out);
-			newline(out);	
+			newline(out);
 		}
-		
+
 		printDefinition(archetype.getDefinition(), out);
 		newline(out);
-		
+
 		printOntology(archetype.getOntology(), out);
 		out.flush();
 		out.close();
 	}
-	
+
 	protected void printHeader(String adlVersion,
-			ArchetypeID id, ArchetypeID parentId, ObjectID uid,
-			String conceptCode, Writer out) throws IOException {
+							   ArchetypeID id, ArchetypeID parentId, ObjectID uid,
+							   String conceptCode, Writer out) throws IOException {
 
 		out.write("archetype");
-        if(StringUtils.isNotEmpty(adlVersion) || (uid != null && StringUtils.isNotEmpty(uid.toString()))) {
-            out.write(" (");  
-        }
+		if(StringUtils.isNotEmpty(adlVersion) || (uid != null && StringUtils.isNotEmpty(uid.toString()))) {
+			out.write(" (");
+		}
 
 		if(StringUtils.isNotEmpty(adlVersion)) {
 			out.write("adl_version=");
@@ -151,11 +151,11 @@ public class ADLSerializer {
 				out.write("; ");
 			}
 			out.write("uid=");
-            out.write(uid.toString());
-        }
-	    if(StringUtils.isNotEmpty(adlVersion) || (uid!=null &&StringUtils.isNotEmpty(uid.toString()))) {
-	        out.write(")");  
-	    }
+			out.write(uid.toString());
+		}
+		if(StringUtils.isNotEmpty(adlVersion) || (uid!=null &&StringUtils.isNotEmpty(uid.toString()))) {
+			out.write(")");
+		}
 		newline(out);
 		indent(1, out);
 		out.write(id.toString());
@@ -176,9 +176,9 @@ public class ADLSerializer {
 		out.write("[" + conceptCode + "]");
 		newline(out);
 	}
-	
+
 	protected void printLanguage(AuthoredResource authored,
-			Writer out) throws IOException {
+								 Writer out) throws IOException {
 
 		out.write("language");
 		newline(out);
@@ -195,18 +195,17 @@ public class ADLSerializer {
 			indent(1, out);
 			out.write("translations = <");
 			newline(out);
-			Map<String, TranslationDetails> translations = 
-				authored.getTranslations();
-			for(Map.Entry<String,TranslationDetails> entry: translations.entrySet()) {
-				String lang = entry.getKey();
-				TranslationDetails td = entry.getValue();
+			Map<String, TranslationDetails> translations =
+					authored.getTranslations();
+			for(String lang : translations.keySet()) {
+				TranslationDetails td = translations.get(lang);
 
 				indent(2, out);
 				out.write("[");
 				out.write(quoteString(lang));
 				out.write("] = <");
 				newline(out);
-				
+
 				indent(3, out);
 				out.write("language = <");
 				out.write("[");
@@ -216,15 +215,15 @@ public class ADLSerializer {
 				out.write("]");
 				out.write(">");
 				newline(out);
-				
+
 				indent(3, out);
 				out.write("author = <");
-				newline(out);				
-				printMap(td.getAuthor(), out, 4);				
+				newline(out);
+				printMap(td.getAuthor(), out, 4);
 				indent(3, out);
 				out.write(">");
 				newline(out);
-				
+
 				if(td.getAccreditation() != null) {
 					indent(3, out);
 					out.write("accreditation = <");
@@ -232,19 +231,19 @@ public class ADLSerializer {
 					out.write(">");
 					newline(out);
 				}
-				
+
 				if(td.getOtherDetails() != null) {
 					indent(3, out);
 					out.write("other_details = <");
-					newline(out);				
-					printMap(td.getOtherDetails(), out, 4);				
+					newline(out);
+					printMap(td.getOtherDetails(), out, 4);
 					indent(3, out);
 					out.write(">");
 					newline(out);
 				}
-				
+
 				indent(2, out);
-				out.write(">");				
+				out.write(">");
 				newline(out);
 			}
 			indent(1, out);
@@ -252,18 +251,18 @@ public class ADLSerializer {
 			newline(out);
 		}
 	}
-	
-	protected void printMap(Map<String,String> map, Writer out, int indent) 
+
+	protected void printMap(Map<String,String> map, Writer out, int indent)
 			throws IOException {
 		if(map == null || map.size() == 0) {
 			return;
 		}
-		for(String key : map.keySet()) {
+		for(Map.Entry<String, String> entry: map.entrySet()) {
 			indent(indent, out);
 			out.write("[");
-			out.write(quoteString(key));
+			out.write(quoteString(entry.getKey()));
 			out.write("] = <");
-			out.write(quoteString(map.get(key)));
+			out.write(quoteString(entry.getValue()));
 			out.write(">");
 			newline(out);
 		}
@@ -283,9 +282,9 @@ public class ADLSerializer {
 		out.write("original_author = <");
 		newline(out);
 		Map<String, String> map = description.getOriginalAuthor();
-		for (String key : map.keySet()) {
+		for (Map.Entry<String, String> entry: map.entrySet()) {
 			indent(2, out);
-			out.write("[" + quoteString(key) + "] = <" + quoteString(map.get(key)) + ">");
+			out.write("[" + quoteString(entry.getKey()) + "] = <" + quoteString(entry.getValue()) + ">");
 			newline(out);
 		}
 		indent(1, out);
@@ -299,7 +298,7 @@ public class ADLSerializer {
 		newline(out);
 
 		printNonEmptyString("resource_package_uri", description.getResourcePackageUri(), 1, out);
-		
+
 		indent(1, out);
 		out.write("details = <");
 		newline(out);
@@ -312,7 +311,7 @@ public class ADLSerializer {
 	}
 
 	protected void printDescriptionItem(ResourceDescriptionItem item,
-			int indent, Writer out) throws IOException {
+										int indent, Writer out) throws IOException {
 		indent(indent, out);
 		out.write("[");
 		out.write(quoteString(item.getLanguage().getCodeString()));
@@ -327,7 +326,7 @@ public class ADLSerializer {
 		out.write(item.getLanguage().getCodeString());
 		out.write("]>");
 		newline(out);
-		
+
 		printNonEmptyString("purpose", item.getPurpose(), indent + 1, out);
 		printNonEmptyStringList("keywords", item.getKeywords(), indent + 1,
 				out);
@@ -344,7 +343,7 @@ public class ADLSerializer {
 	}
 
 	private void printNonEmptyString(String label, String value, int indent,
-			Writer out) throws IOException {
+									 Writer out) throws IOException {
 
 		if (StringUtils.isEmpty(value)) {
 			return;
@@ -358,7 +357,7 @@ public class ADLSerializer {
 	}
 
 	private void printNonEmptyStringList(String label, List<String> list,
-			int indent, Writer out) throws IOException {
+										 int indent, Writer out) throws IOException {
 
 		if (list == null || list.isEmpty()) {
 			return;
@@ -377,7 +376,7 @@ public class ADLSerializer {
 	}
 
 	private void printNonEmptyStringMap(String label, Map<String, String> map,
-			int indent, Writer out) throws IOException {
+										int indent, Writer out) throws IOException {
 		if (map == null || map.isEmpty()) {
 			return;
 		}
@@ -387,9 +386,9 @@ public class ADLSerializer {
 		out.write(" = <");
 		newline(out);
 
-		for (String key : map.keySet()) {
+		for (Map.Entry<String, String> entry: map.entrySet()) {
 			indent(2, out);
-			out.write("[" + quoteString(key) + "] = <" + quoteString(map.get(key)) + ">");
+			out.write("[" + quoteString(entry.getKey()) + "] = <" + quoteString(entry.getValue()) + ">");
 			newline(out);
 		}
 
@@ -408,16 +407,16 @@ public class ADLSerializer {
 	}
 
 	protected void printCComplexObject(CComplexObject ccobj, int indent,
-			Writer out) throws IOException {
-		
+									   Writer out) throws IOException {
+
 		// TODO skip c_obj with [0,0] occurrences
 		Interval<Integer> occurrences = ccobj.getOccurrences();
-		if(occurrences != null 
+		if(occurrences != null
 				&& (Integer.valueOf(0).equals(occurrences.getLower()))
 				&& (Integer.valueOf(0).equals(occurrences.getUpper()))) {
-			return;		
+			return;
 		}
-						
+
 
 		// print rmTypeName and nodeId
 		indent(indent, out);
@@ -469,12 +468,12 @@ public class ADLSerializer {
 	}
 
 	protected void printArchetypeInternalRef(ArchetypeInternalRef ref,
-			int indent, Writer out) throws IOException {
+											 int indent, Writer out) throws IOException {
 		indent(indent, out);
 		out.write("use_node ");
 		out.write(ref.getRmTypeName());
 		printOccurrences(ref.getOccurrences(), out);
-		out.write(" ");		
+		out.write(" ");
 		out.write(ref.getTargetPath());
 		newline(out);
 	}
@@ -503,33 +502,33 @@ public class ADLSerializer {
 			}
 			newline(out);
 			indent(indent, out);
-			out.write("}");			
+			out.write("}");
 		}
 		newline(out);
 	}
-	
+
 	private void printAssertions(Set<Assertion> assertions, String purpose,
-			int indent, Writer out)	throws IOException {
+								 int indent, Writer out)	throws IOException {
 
 		if(assertions == null) {
 			return;
 		}
-		
+
 		newline(out);
 		indent(indent + 1, out);
 		out.write(purpose);
-		
+
 		for (Assertion assertion : assertions) {
-			
+
 			if(assertion.getStringExpression() == null) {
 				continue;
 			}
-			
+
 			newline(out);
 			indent(indent + 2, out);
-			
-			// FIXME: The string expression is null when an archetype is parsed, but after the archetype is recreated in the archetype 
-			// editor, the string expression exists. Please provide a valid string expression from the parser since it's _much_ easier to 
+
+			// FIXME: The string expression is null when an archetype is parsed, but after the archetype is recreated in the archetype
+			// editor, the string expression exists. Please provide a valid string expression from the parser since it's _much_ easier to
 			// maintain this line of code instead of adding hundreds of lines just to output some expressions, operators etc.
 			// Opening an archetype directly in the ADL format view will show the output of the parsed archetype in this way:
 			//
@@ -552,7 +551,7 @@ public class ADLSerializer {
 			CMultipleAttribute cma = (CMultipleAttribute) cattribute;
 			if(cma.getCardinality() != null) {
 				out.write(" ");
-			    printCardinality(cma.getCardinality(), out);
+				printCardinality(cma.getCardinality(), out);
 			}
 		}
 		List<CObject> children = cattribute.getChildren();
@@ -604,7 +603,7 @@ public class ADLSerializer {
 			printConstraintRef((ConstraintRef) cobj, indent, out);
 		}
 	}
-	
+
 	protected void printConstraintRef(ConstraintRef ref, int indent, Writer out) throws IOException {
 		indent(indent, out);
 		out.write("[");
@@ -650,9 +649,9 @@ public class ADLSerializer {
 			printCDvOrdinal((CDvOrdinal) cdomain, indent, out);
 		} else if (cdomain instanceof CDvQuantity) {
 			printCDvQuantity((CDvQuantity) cdomain, indent, out);
-		}  
-		 else if (cdomain instanceof CCodePhrase) {
-		    printCCodePhrase((CCodePhrase) cdomain, indent, out);
+		}
+		else if (cdomain instanceof CCodePhrase) {
+			printCCodePhrase((CCodePhrase) cdomain, indent, out);
 		}
 		// unknow CDomainType
 	}
@@ -661,7 +660,7 @@ public class ADLSerializer {
 			throws IOException {
 
 		indent(indent, out);
-		
+
 		if(ccoded.isAnyAllowed()) {
 			out.write("C_CODE_PHRASE <");
 			newline(out);
@@ -704,7 +703,7 @@ public class ADLSerializer {
 				}
 				out.write("]");
 				newline(out);
-			} 
+			}
 		} else {
 			out.write("]");
 			newline(out);
@@ -725,9 +724,9 @@ public class ADLSerializer {
 			newline(out);
 		}
 		else {
-			for (Iterator<Ordinal> it = cordinal.getList().iterator(); 
-			it.hasNext();) {
-				Ordinal ordinal = it.next();			
+			for (Iterator<Ordinal> it = cordinal.getList().iterator();
+				 it.hasNext();) {
+				Ordinal ordinal = it.next();
 				indent(indent, out);
 				printOrdinal(ordinal, out);
 				if (it.hasNext()) {
@@ -735,17 +734,17 @@ public class ADLSerializer {
 				} else if(cordinal.hasAssumedValue()) {
 					out.write(";");
 				}
-				newline(out);			
+				newline(out);
 			}
 			if(cordinal.hasAssumedValue()) {
 				printOrdinal(cordinal.getAssumedValue(), out);
 				newline(out);
 
 			}
-		}	
+		}
 	}
 
-	protected void printOrdinal(Ordinal ordinal, Writer out) 
+	protected void printOrdinal(Ordinal ordinal, Writer out)
 			throws IOException {
 		CodePhrase symbol = ordinal.getSymbol();
 		out.write(Integer.toString(ordinal.getValue()));
@@ -753,11 +752,11 @@ public class ADLSerializer {
 		out.write(symbol.getTerminologyId().getValue());
 		out.write("::");
 		out.write(symbol.getCodeString());
-		out.write("]");		
+		out.write("]");
 	}
 
 	protected void printCDvQuantity(CDvQuantity cquantity, int indent,
-			Writer out) throws IOException {
+									Writer out) throws IOException {
 		indent(indent, out);
 		out.write("C_DV_QUANTITY <");
 		newline(out);
@@ -772,7 +771,7 @@ public class ADLSerializer {
 		}
 		List<CDvQuantityItem> list = cquantity.getList();
 		if (list != null) {
-			newline(out);			
+			newline(out);
 			indent(indent + 1, out);
 			out.write("list = <");
 			newline(out);
@@ -796,7 +795,7 @@ public class ADLSerializer {
 					out.write(">");
 					newline(out);
 				}
-				
+
 				Interval<Integer> precision = item.getPrecision();
 				if (precision != null) {
 					indent(indent + 3, out);
@@ -814,10 +813,10 @@ public class ADLSerializer {
 			out.write(">");
 			newline(out);
 		}
-		
-		
+
+
 		if(cquantity.getAssumedValue() != null) {
-			newline(out);			
+			newline(out);
 			indent(indent + 1, out);
 			out.write("assumed_value = <");
 			newline(out);
@@ -825,20 +824,20 @@ public class ADLSerializer {
 			indent(indent + 1, out);
 			out.write(">");
 			newline(out);
-		}		
-		
+		}
+
 		indent(indent, out);
 		out.write(">");
 		newline(out);
 	}
-	
-	protected void printDvQuantity(DvQuantity quantity, int indent, Writer out) 
+
+	protected void printDvQuantity(DvQuantity quantity, int indent, Writer out)
 			throws IOException {
-		
+
 		indent(indent + 1, out);
 		printUnits(quantity.getUnits(), out);
 		newline(out);
-		
+
 		if(quantity.getMagnitude() != null) {
 			indent(indent + 1, out);
 			out.write("magnitude = <");
@@ -852,7 +851,7 @@ public class ADLSerializer {
 		out.write(">");
 		newline(out);
 	}
-	
+
 	protected void printUnits(String units, Writer out) throws IOException {
 		out.write("units = <");
 		out.write(quoteString(units));
@@ -927,8 +926,8 @@ public class ADLSerializer {
 
 					if (item.getTerms().size() > 1) {
 						for (int k = 1; k < item.getTerms().size(); k++) {
-                            out.write("," + item.getTerms().get(k));
-                        }
+							out.write("," + item.getTerms().get(k));
+						}
 					}
 
 					out.write(">");
@@ -987,12 +986,12 @@ public class ADLSerializer {
 		}
 	}
 
-    private String quoteString(String value) {
-        return "\"" + value.replaceAll("[\"]", "\\\\$0") + "\"";
-    }
+	private String quoteString(String value) {
+		return "\"" + value.replaceAll("[\"]", "\\\\$0") + "\"";
+	}
 
 	private void printDefinitionList(Writer out,
-			List<OntologyDefinitions> termDefinitionsList) throws IOException {
+									 List<OntologyDefinitions> termDefinitionsList) throws IOException {
 		for (OntologyDefinitions defs : termDefinitionsList) {
 			indent(2, out);
 			out.write("[");
@@ -1070,7 +1069,7 @@ public class ADLSerializer {
 			} else {
 				out.write("false");
 			}
- 		}
+		}
 	}
 
 	protected void printCDate(CDate cdate, Writer out) throws IOException {
@@ -1184,8 +1183,8 @@ public class ADLSerializer {
 			if (string) {
 				out.write(quoteString(item));
 			} else {
-			    out.write(item);
-            }
+				out.write(item);
+			}
 		}
 	}
 
@@ -1194,7 +1193,7 @@ public class ADLSerializer {
 		out.write("|");
 		if (interval.getLower() != null && interval.getUpper() != null) {
 			if(interval.getLower().equals(interval.getUpper())
-					&& interval.isLowerIncluded() 
+					&& interval.isLowerIncluded()
 					&& interval.isUpperIncluded()) {
 				out.write(interval.getLower().toString());
 			} else {
@@ -1239,27 +1238,27 @@ public class ADLSerializer {
 }
 /*
  * ***** BEGIN LICENSE BLOCK ***** Version: MPL 1.1/GPL 2.0/LGPL 2.1
- * 
+ *
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 (the 'License'); you may not use this file except in compliance with the
  * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an 'AS IS' basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
  * the specific language governing rights and limitations under the License.
- * 
+ *
  * The Original Code is ADLSerializer.java
- * 
+ *
  * The Initial Developer of the Original Code is Rong Chen. Portions created by
  * the Initial Developer are Copyright (C) 2004-2007 the Initial Developer. All
  * Rights Reserved.
- * 
- * Contributor(s): Mattias Forss, Johan Hjalmarsson, Erik Sundvall, 
+ *
+ * Contributor(s): Mattias Forss, Johan Hjalmarsson, Erik Sundvall,
  *                 Sebastian Garde
- * 
+ *
  * Software distributed under the License is distributed on an 'AS IS' basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
  * the specific language governing rights and limitations under the License.
- * 
+ *
  * ***** END LICENSE BLOCK *****
  */

measure-serv/src/main/java/org/openehr/rm/support/measurement/MeasurementService.java
@@ -33,7 +33,7 @@ public interface MeasurementService extends Serializable{
      * @return true if units valid
      * @throws IllegalArgumentException if units null
      */
-    public boolean isValidUnitsString(String units);
+    boolean isValidUnitsString(String units);
 
     /**
      * Return True if two units strings correspond to the same
@@ -44,7 +44,7 @@ public interface MeasurementService extends Serializable{
      * @return true if two units equal
      * @throws IllegalArgumentException if units1 or units2 null
      */
-    public boolean unitsEquivalent(String units1, String units2);
+    boolean unitsEquivalent(String units1, String units2);
     
     /**
      * Return True if two units strings are comparable.
@@ -54,7 +54,7 @@ public interface MeasurementService extends Serializable{
      * @return true if two units comparable
      * @throws IllegalArgumentException if units1 or units2 null
      */
-    public boolean unitsComparable(String units1, String units2);
+    boolean unitsComparable(String units1, String units2);
     
     /**
      * Comparison between two measures.
@@ -66,7 +66,7 @@ public interface MeasurementService extends Serializable{
      *         quantity.
      * @throws IllegalArgumentException if units1, units2 null or not comparable
      */
-    public int compare(String units1, Double value1, String units2, Double value2);
+    int compare(String units1, Double value1, String units2, Double value2);
 }
 /*
  *  ***** BEGIN LICENSE BLOCK *****

measure-serv/src/main/java/org/openehr/rm/support/measurement/SimpleMeasurementService.java
@@ -29,7 +29,7 @@ import javax.measure.unit.Unit;
  * @version 1.0
  *
  */
-public class  SimpleMeasurementService implements MeasurementService {
+public final class  SimpleMeasurementService implements MeasurementService {
 
     /**
      * 

mini-termserv/src/main/java/org/openehr/terminology/SimpleTerminologyAccess.java
@@ -57,12 +57,13 @@ public class SimpleTerminologyAccess implements TerminologyAccess {
 			group.add(code);
 		}
 		groups.put(groupId, group);
-		for(String lang : names.keySet()) {
+		for(Map.Entry<String, String> entry : names.entrySet()) {
+			String lang = entry.getKey();
 			Map<String, String> nameToId = groupLangNameToId.get(lang);
 			if(nameToId == null) {
 				nameToId = new HashMap<String, String>();
 			}
-			String name = names.get(lang);
+			String name = entry.getValue();
 			
 			nameToId.put(name, groupId);
 			groupLangNameToId.put(lang, nameToId);

openehr-aom/src/main/java/org/openehr/am/archetype/assertion/ExpressionLeaf.java
@@ -45,7 +45,7 @@ public class ExpressionLeaf extends ExpressionItem {
 	 * @return
 	 */
 	public static final ExpressionLeaf intConstant(int i) {
-		return new ExpressionLeaf(ExpressionItem.INTEGER, new Integer(i),
+		return new ExpressionLeaf(ExpressionItem.INTEGER, Integer.valueOf(i),
 				ExpressionLeaf.ReferenceType.CONSTANT);
 	}
 	
@@ -56,7 +56,7 @@ public class ExpressionLeaf extends ExpressionItem {
 	 * @return
 	 */
 	public static final ExpressionLeaf realConstant(double d) {
-		return new ExpressionLeaf(ExpressionItem.REAL, new Double(d),
+		return new ExpressionLeaf(ExpressionItem.REAL, Double.valueOf(d),
 				ExpressionLeaf.ReferenceType.CONSTANT);
 	}
 	

openehr-aom/src/main/java/org/openehr/am/archetype/constraintmodel/CAttribute.java
@@ -75,8 +75,9 @@ public abstract class CAttribute extends ArchetypeConstraint {
     	for(CObject cobj : children) {
     		if(cobj == null) {
     			System.out.println("null cobj while copying c_attr: " + rmAttributeName + ", " + path());    			
-    		}
-    		list.add(cobj.copy());    		
+    		} else {
+                list.add(cobj.copy());
+            }
     	}    	
     	return list;
     }

openehr-ap/src/main/java/org/openehr/am/openehrprofile/datatypes/quantity/CDvQuantity.java
@@ -56,8 +56,9 @@ public class CDvQuantity extends CDomainType<DvQuantity> {
 		if (list != null && list.isEmpty()) {
 			throw new IllegalArgumentException("empty list");
 		}
-		this.list = list == null ? list 
-				: new ArrayList<CDvQuantityItem>(list);
+		if(list != null) {
+			this.list = new ArrayList<CDvQuantityItem>(list);
+		}
 		this.property = property;
 	}
 	

commit 1423cc89928e1ec14552f6117c3d4ea655b8d156

Fix for bug introduced in commit 88a9200 - otherwise XMLSerializer never serialises the text or description of at-codes (and other string maps)

xml-serializer/src/main/java/org/openehr/am/serialize/XMLSerializer.java
@@ -8,7 +8,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import org.jdom.Document;
 import org.jdom.Element;
@@ -166,7 +165,7 @@ public class XMLSerializer {
         printString("value", archetype.getArchetypeId().toString(), archetypeId);
 
         printString("adl_version", archetype.getAdlVersion(), out);
-      
+
 
         printString("concept", archetype.getConcept(), out);
 
@@ -268,7 +267,7 @@ public class XMLSerializer {
                 out.getChildren().add(elm);
                 elm.setAttribute("id", entry.getKey());
                 if (entry.getValue() != null) {
-                    elm.setText(map.get(entry.getValue()));
+                    elm.setText(entry.getValue());
                 }
             }
         }
@@ -1101,14 +1100,14 @@ public class XMLSerializer {
             char c = str.charAt( i );
             if(! Character.isUpperCase(prevChar) &&
                     !(prevChar=='<') && // without this DV_INTERVAL<DV_QUANTITY>    -->    DV_INTERVAL<_DV_QUANTITY>
-                    !(prevChar=='_') && 
+                    !(prevChar=='_') &&
                     Character.isLetter(c) &&
                     Character.isUpperCase(c))
             {
                 result.append("_");
                 result.append( Character.toUpperCase( c ) );
             }
-            else { 
+            else {
                 result.append( Character.toUpperCase( c ) );
             }
             prevChar = c;
@@ -1131,7 +1130,7 @@ public class XMLSerializer {
 
     public static final Namespace xsdNamespace = Namespace.getNamespace("xsd", "http://www.w3.org/2001/XMLSchema");
 
-    /* fields */  
+    /* fields */
     private Charset encoding;
 
     private XMLOutputter outputter;